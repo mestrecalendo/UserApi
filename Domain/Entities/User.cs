80cee5b7b8cdc1381a3371409b847ab1132b630b
Sofia Reis <57951365+mestrecalendo@users.noreply.github.com>
2023-09-23 17:10:55 -0400

    Adicionado validação de idade

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Domain.Entities
 {
@@ -13,7 +14,38 @@ namespace Domain.Entities
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Email { get; set; }
-        public DateTime DataNascimento { get; set; }
+
+        private DateTime _dataNascimento;
+        public DateTime DataNascimento {
+            get { return _dataNascimento; }
+
+            set {
+                if (!this.ValidaIdade(value))
+                {
+                   throw new FormatException("A idade do usuário não pode ser menor que 15 anos");
+                }
+                _dataNascimento = value;
+            } 
+        }
         public virtual EscolaridadeType IdEscolaridade { get; set; }
+
+        private bool ValidaIdade(DateTime data)
+        {
+            var dataNascimento = Convert.ToDateTime(data);
+            var IdadeDoUsuario = DateTime.Today.Year - dataNascimento.Year;
+
+            if (dataNascimento > DateTime.Today.AddYears(-IdadeDoUsuario))
+            {
+                IdadeDoUsuario--;
+            }
+
+            if (IdadeDoUsuario <= 15)
+            {
+               return false;
+            }
+
+            return true;
+        }
+
     }
 }
